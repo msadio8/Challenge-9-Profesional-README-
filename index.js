20ef82dfc7f8c9f515e0efe554868668cf94867d
Meriam <132971614+msadio8@users.noreply.github.com>
2023-08-03 22:52:49 +1000

    add code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
+const { default: Choices } = require("inquirer/lib/objects/choices");
+const { argv0 } = require("process");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -27,6 +29,78 @@ const questions = [
         return true;
       } else {
         console.log("description is required !");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "Table",
+    message: "Please provide table of Contents for this Project?",
+    Validate: (TableInput) => {
+      if (TableInput) {
+        return true;
+      } else {
+        console.log("Please enter table content!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "Installation",
+    message: "Please Provide Installation instruction",
+    Validate: (InstallationInput) => {
+      if (InstallationInput) {
+        return true;
+      } else {
+        console.log("Please input installation instruction!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "Please Provide Usage instruction for this Project?",
+    Validate: (UsageInput) => {
+      if (UsageInput) {
+        return true;
+      } else {
+        console.log("Please input Usage instruction!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "please select license for this project?"
+    choices: ["apache", "agpl","mit", "no lincense"],
+    
+  },
+  {
+    type: "input",
+    name: "Contribution",
+    message: "Please input any contribution guidlines for this project",
+    Validate: (ContributionInput) => {
+      if (ContributionInput) {
+        return true;
+      } else {
+        console.log("Contribution guidelines is require!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please enter test instruction",
+    Validate: (testInput) => {
+      if (testInput) {
+        return true;
+      } else {
+        console.log("Please enter test insttruction !");
         return false;
       }
     },
